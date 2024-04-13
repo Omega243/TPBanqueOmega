65ab267a9f61d5dd23da49af5f37cde1a704387a
Omega243 <140796147+Dina243@users.noreply.github.com>
2024-04-13 22:22:47 +0300

    Code du bean CDI (création compte bancaire/retourner la liste de tous les comptes bancaires)

--- src/main/java/mg/itu/tpbanqueomega/entity/CompteBancaire.java
+++ src/main/java/mg/itu/tpbanqueomega/entity/CompteBancaire.java
@@ -8,6 +8,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.NamedQueries;
+import jakarta.persistence.NamedQuery;
+import jakarta.persistence.Table;
+import jakarta.xml.bind.annotation.XmlRootElement;
 import java.io.Serializable;
 
 /**
@@ -15,6 +19,10 @@ import java.io.Serializable;
  * @author Dina
  */
 @Entity
+@Table(name = "customer")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "CompteBancaire.findAll", query = "SELECT c FROM CompteBancaire c")})
 public class CompteBancaire implements Serializable {
 
     private static final long serialVersionUID = 1L;
--- src/main/java/mg/itu/tpbanqueomega/service/GestionnaireCompte.java
+++ src/main/java/mg/itu/tpbanqueomega/service/GestionnaireCompte.java
@@ -6,6 +6,13 @@ package mg.itu.tpbanqueomega.service;
 
 import jakarta.annotation.sql.DataSourceDefinition;
 import jakarta.enterprise.context.RequestScoped;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
+import jakarta.persistence.TypedQuery;
+import jakarta.transaction.Transactional;
+import java.util.List;
+import mg.itu.tpbanqueomega.entity.CompteBancaire;
 
 /**
  *
@@ -27,5 +34,17 @@ import jakarta.enterprise.context.RequestScoped;
 )
 @RequestScoped
 public class GestionnaireCompte {
+    @PersistenceContext(unitName = "banquePU")
+    private EntityManager em;
+    
+    public List<CompteBancaire> getAllComptes() {
+       TypedQuery<CompteBancaire> query = em.createNamedQuery("CompteBancaire.findAll", CompteBancaire.class);
+       return query.getResultList();
+    }
+    
+    @Transactional
+    public void creerCompte(CompteBancaire c) {
+      em.persist(c);
+    }
     
 }
